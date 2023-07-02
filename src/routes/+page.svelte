243b32a77b35266fa064a825e46d8b86303aef04
Håvard Johansen <havard@findmy.no>
2023-07-03 00:27:50 +0200

    cleanup and simplification

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,15 @@
 <script lang="ts">
-	import { afterUpdate, onMount } from 'svelte';
+	import { onMount } from 'svelte';
 
 	import { gameStore } from '../stores/gameStore';
 
-	import type { GameStore, ResourceName, UpgradeName } from '../stores/gameStore';
-
-	let gameState: GameStore;
-	let canAffordUpgrade1 = false;
-	let canAffordUpgrade2 = false;
-
 	onMount(() => {
-		const unsubscribe = gameStore.subscribe((value) => {
-			gameState = value;
-		});
-
 		startGameLoop();
-
-		return unsubscribe;
-	});
-
-	afterUpdate(() => {
-		canAffordUpgrade1 = canAffordUpgrade('upgrade1', 'rock', 10);
-		canAffordUpgrade2 = canAffordUpgrade('upgrade2', 'rock', 100);
 	});
 
 	// Game loop
 	function startGameLoop(): void {
-		const updateInterval = 1000 / 30; // 60 FPS
+		const updateInterval = 1000 / 10; // 30 FPS
 
 		setInterval(() => {
 			// Game logic
@@ -36,77 +19,19 @@
 
 	// Update game logic
 	function updateGameLogic(): void {
-		incrementResource('rock', 0.1);
-	}
-
-	// Increment the resource by a specified amount
-	function incrementResource(resource: ResourceName, amount: number): void {
-		gameStore.update((state: GameStore) => {
-			return {
-				...state,
-				resources: {
-					...state.resources,
-					[resource]: state.resources[resource] + amount
-				}
-			};
-		});
-	}
-
-	// Purchase an upgrade
-	function purchaseUpgrade(upgrade: UpgradeName, resource: ResourceName, cost: number): void {
-		if (gameState) {
-			const canAfford = gameState.resources[resource] >= cost;
-
-			if (canAfford) {
-				gameStore.update((state: GameStore) => {
-					return {
-						...state,
-						resources: {
-							...state.resources,
-							[resource]: state.resources[resource] - cost
-						},
-						upgrades: {
-							...state.upgrades,
-							[upgrade]: true
-						}
-					};
-				});
-			}
-		}
-	}
-
-	// Check if the player can afford an upgrade
-	function canAffordUpgrade(upgrade: UpgradeName, resource: ResourceName, cost: number): boolean {
-		const resourceValue = gameState.resources[resource];
-		return resourceValue >= cost;
+		$gameStore.resources.rock += 1;
 	}
 </script>
 
 <main>
-	<h1>Incremental Game</h1>
-	{#if gameState}
-		<p>Rocks: {Math.floor(gameState.resources.rock)}</p>
-		<p>Diamonds: {Math.floor(gameState.resources.diamonds)}</p>
-		<h2>Upgrades</h2>
-		<div class="flex gap-2">
-			<button
-				class="btn btn-success"
-				on:click={() => purchaseUpgrade('upgrade1', 'rock', 10)}
-				disabled={!canAffordUpgrade1}>Upgrade 1 (Cost: 10)</button
-			>
-			<button
-				class="btn btn-success"
-				on:click={() => purchaseUpgrade('upgrade2', 'rock', 100)}
-				disabled={!canAffordUpgrade2}>Upgrade 2 (Cost: 100)</button
-			>
-		</div>
-	{/if}
+	<h1 class="text-3xl text-center">Space Mining Corp</h1>
+	<pre>{JSON.stringify($gameStore, null, 2)}</pre>
+	<div>
+		<span>Rock: {$gameStore.resources.rock}</span>
+	</div>
+	<div class="flex gap-2 flex-col">
+		<span>Miners: {$gameStore.crew.miners}</span>
+		<span>Mechanics: {$gameStore.crew.mechanics}</span>
+		<span>Soldiers: {$gameStore.crew.soldiers}</span>
+	</div>
 </main>
-
-<style>
-	main {
-		max-width: 800px;
-		margin: 0 auto;
-		padding: 2rem;
-	}
-</style>
--- src/stores/gameStore.ts
+++ src/stores/gameStore.ts
@@ -1,29 +1,18 @@
 import { writable } from 'svelte/store';
 
-export type ResourceName = 'rock' | 'diamond';
-export type UpgradeName = 'upgrade1' | 'upgrade2';
-
-export interface Resources {
-	[key: string]: number;
-}
-
-export interface GameStore {
-	resources: Resources;
-	upgrades: Upgrades;
-}
-
-export interface Upgrades {
-	[key: string]: boolean;
-}
-
-const initialState: GameStore = {
+const initialState = {
 	resources: {
 		rock: 0,
 		diamonds: 0
 	},
+	crew: {
+		miners: 0,
+		mechanics: 0,
+		soldiers: 0
+	},
 	upgrades: {
-		upgrade1: false,
-		upgrade2: false
+		automatedMiner: false,
+		mechanicWorkshop: false
 	}
 };
 
